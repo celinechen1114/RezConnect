75bd5eaf9689af6fab07a4a2c2c3c9f6550c2334
yoonseok312 <yoonseok@berkeley.edu>
2023-08-07 15:31:24 -0700

    :sparkles: Add profile button
    add profile button

--- res-connect/src/App.js
+++ res-connect/src/App.js
@@ -1,11 +1,27 @@
 import logo from "./background.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Profile() {
+  return (
+    <button
+      className="absolute top-4 right-4 bg-white bg-opacity-50 p-2"
+      onClick={() => {
+        alert("Profile button clicked!");
+      }}
+    >
+      {/* <img src={profilePic} alt="Profile Pic" /> */}
+      <div>Name: John Doe</div>
+      <div>Grade: Freshman</div>
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="bg-cover" alt="logo" />
+        <Profile />
       </header>
     </div>
   );
